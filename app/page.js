cc5830555f205f416610b821825c21234bed4185
Ayush Raj <119657235+rajayush01@users.noreply.github.com>
2024-12-24 13:10:16 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,12 @@
 "use client";
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import dynamic from 'next/dynamic';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Home/Homepage';
 import Login from './components/Login/Login';
 import Signup from './components/SignUp/SignUp';
 import About from './pages/About/About';
 import Contact from './pages/Contact/Contact';
-import DoctorDashboard from './pages/Dashboard/DashboardDoc'
-import PatientDashboard from './pages/Dashboard/DashboardPatient';
 import PatientList from './components/DashboardComponents/Patients/PatientList';
 import AppointmentScheduling from './components/DashboardComponents/AppointmentScheduling';
 import MedicalReports from './components/DashboardComponents/MedicalReports';
@@ -16,10 +15,12 @@ import PatientProfile from './pages/Profile/PatientProfile';
 import NotificationSystem from './components/DashboardComponents/NotificationSystem';
 import SettingsComponent from './components/DashboardComponents/Settings';
 import VideoCall from './components/VideoCall/VideoCall';
-import DashboardPatient from './pages/Dashboard/DashboardPatient';
 import AppointmentPortal from './components/DashboardComponents/Patients/PatientAppointment';
 import ReportPage from './components/DashboardComponents/Patients/PatientReport';
 import EditablePatient from './pages/Profile/EditablePatientProfile';
+
+
+const DashboardWrapper = dynamic(() => import('./DashboardWrapper'), { ssr: false });
 
 function App() {
   return (
@@ -35,13 +36,13 @@ function App() {
           <Route path="/schedule" element={<AppointmentScheduling />} />
           <Route path="/appointment" element={<AppointmentPortal />} />
           <Route path="/reports" element={<MedicalReports />} />
-          <Route path="/patreport" element={<ReportPage/>} />
-          <Route path="/docpro/:id" element={<DoctorProfile />} /> 
+          <Route path="/patreport" element={<ReportPage />} />
+          <Route path="/docpro/:id" element={<DoctorProfile />} />
           <Route path="/patpro/:id" element={<PatientProfile />} />
           <Route path="/notification" element={<NotificationSystem />} />
           <Route path="/setting" element={<SettingsComponent />} />
           <Route path="/video" element={<VideoCall />} />
-          <Route path="/edit/:id" element={<EditablePatient/>} />
+          <Route path="/edit/:id" element={<EditablePatient />} />
           {/* Conditional Dashboard Rendering */}
           <Route path="/dashboard" element={<DashboardWrapper />} />
         </Routes>
@@ -62,26 +63,5 @@ function App() {
   );
 }
 
-
-const DashboardWrapper = () => {
-  const [userType, setUserType] = useState(null);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const searchParams = new URLSearchParams(window.location.search);
-      setUserType(searchParams.get('userType'));
-    }
-  }, []);
-
-  if (userType === 'doctor') {
-    return <DoctorDashboard />;
-  } else if (userType === 'patient') {
-    return <PatientDashboard />;
-  } else if (userType === null) {
-    return <div>Loading...</div>; // Display a loading state until userType is determined
-  } else {
-    return <div>No user type specified. Please login again.</div>;
-  }
-};
 
 export default App;
